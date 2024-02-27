d82e7a0c55ea0a1abc34f967f9fb7b1088457198
Abhinaya Aryal <aryalabhinaya343@gmail.com>
2024-02-27 17:31:24 +0545

    initialize ui and css

--- main.c
+++ main.c
@@ -1,24 +1,32 @@
 #include <gtk/gtk.h>
 
 static void activate(GtkApplication *app, gpointer user_data) {
-  GtkWidget *window;
-  GtkWidget *button;
+  GtkBuilder *builder;
+  GtkCssProvider *provider;
 
-  window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW(window), "SeamlessSync");
-  gtk_window_set_default_size(GTK_WINDOW(window), 500, 400);
+  builder = gtk_builder_new();
+  gtk_builder_add_from_file(builder, "../ui/builder.ui", NULL);
 
-  gtk_window_present(GTK_WINDOW(window));
+  GObject *window = gtk_builder_get_object(builder, "window");
+  gtk_window_set_application(GTK_WINDOW(window), app);
+
+  provider = gtk_css_provider_new();
+  gtk_css_provider_load_from_file(provider,
+                                  g_file_new_for_path("../css/style.css"));
+  gtk_style_context_add_provider_for_display(
+      gtk_widget_get_display(GTK_WIDGET(window)), GTK_STYLE_PROVIDER(provider),
+      GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+  gtk_widget_set_visible(GTK_WIDGET(window), TRUE);
+  g_object_unref(builder);
 }
 
 int main(int argc, char **argv) {
   GtkApplication *app;
   int status;
-  
+
   app = gtk_application_new("org.seamlessSync", G_APPLICATION_DEFAULT_FLAGS);
-
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-
   status = g_application_run(G_APPLICATION(app), argc, argv);
 
   g_object_unref(app);
