8212caf60e7cc5a8fc31dac360814b2efad0dcab
Abhinaya Aryal <aryalabhinaya343@gmail.com>
2024-07-27 09:20:16 +0545

    thread in ui

--- src/ui/sideBar.c
+++ src/ui/sideBar.c
@@ -2,7 +2,7 @@
 #include "onSidebarItemSelected.c"
 #include "verticalBox.c"
 
-void create_sidebar(GtkWidget *box, GtkWidget *main_content, char *items[],
+void create_sidebar(GtkWidget *h_box, GtkWidget *v_box, char *items[],
                     int n_items) {
   // Create a vertical box to hold the sticky box and the list box
   GtkWidget *sidebar_container = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
@@ -28,16 +28,16 @@ void create_sidebar(GtkWidget *box, GtkWidget *main_content, char *items[],
     gtk_list_box_append(GTK_LIST_BOX(sidebar), row);
   }
   g_signal_connect(sidebar, "row-selected",
-                   G_CALLBACK(on_sidebar_item_selected), main_content);
+                   G_CALLBACK(on_sidebar_item_selected), v_box);
 
   // Make the sidebar scrollable
   GtkWidget *scrolled_window = gtk_scrolled_window_new();
   gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scrolled_window), sidebar);
   gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window),
                                  GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
-  gtk_widget_set_size_request(scrolled_window, 120, -1);
+  gtk_widget_set_size_request(scrolled_window, 150, -1);
   gtk_widget_set_vexpand(scrolled_window, TRUE);
 
   gtk_box_append(GTK_BOX(sidebar_container), scrolled_window);
-  gtk_box_append(GTK_BOX(box), sidebar_container);
+  gtk_box_append(GTK_BOX(h_box), sidebar_container);
 }
