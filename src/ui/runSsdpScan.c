a55cc692fc2a40fb700f327e1fee3d96cf0bc520
Abhinaya Aryal <aryalabhinaya343@gmail.com>
2024-09-10 12:56:18 +0545

    remove timer on recvFrom

--- src/ui/runSsdpScan.c
+++ src/ui/runSsdpScan.c
@@ -3,9 +3,7 @@
 
 static void run_ssdp_scan(GTask *task, gpointer source_object, gpointer data,
                           GCancellable *cancellable) {
-  // UiUpdateData *ui_data = (UiUpdateData *)data;
-
-  struct ssdpMessage *revMsg = SSDPListen(5);
+  struct ssdpMessage *revMsg = SSDPListen(8);
   g_print("Run ssdp listen function\n");
   if (g_task_return_error_if_cancelled(task)) {
     return;
